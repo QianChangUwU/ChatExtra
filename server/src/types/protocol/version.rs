f1a45e3f1e569dd1231ba339fe2a80f40c791159
BaiHanChuan <2318933089@qq.com>
2026-06-20 15:16:17 +0800

    fix: VersionRequest/Response 添加 #[serde(default)] 兼容旧客户端

--- server/src/types/protocol/version.rs
+++ server/src/types/protocol/version.rs
@@ -3,10 +3,13 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct VersionRequest {
     pub version: u32,
+    #[serde(default)]
+    pub client_version: String,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct VersionResponse {
     pub version: u32,
+    #[serde(default)]
     pub required_version: String,
 }
